f615474671c4f24ec5ac34939beb2e5eb80ae762
stive <stivemok@gmail.com>
2023-08-22 00:02:13 +0300

    javascript

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-document.querySelector('.login100-form-btn').addEventListener('click', function(event) {
+/* document.querySelector('.login100-form-btn').addEventListener('click', function(event) {
     event.preventDefault();
     
-    // redirect to admin page
+     redirect to admin page
     window.location.href = 'file:///C:/Users/i/Desktop/portfolio/admin_page.html';
+    window.location.href = `admin_page.html${queryString}`;
+});*/
+// Add an event listener to the login button
+document.getElementById("loginButton").addEventListener("click", function() {
+    // Open admin_page.html in a new window or tab
+    window.open("admin_page.html", "_blank");
 });
